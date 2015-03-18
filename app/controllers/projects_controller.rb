cb0236fb203ab29db523e40510b4ac36fbd45a4c
dphaener <dphaener@gmail.com>
2015-03-17 19:05:28 -0700

    Day-9 half way

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,28 @@ class ProjectsController < ApplicationController
   def index
     @projects = current_user.projects
   end
+
+  def new
+    @project = current_user.projects.new
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @project = Project.find_by_id(params[:id])
+  end
+
+private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
